ba1450360acb09ebc6e18e3429af6f68eed1cdd3
Brayan Acosta <landakkpp@gmail.com>
2024-08-02 17:43:13 -0500

    creating main view

---
+++ Views/MainView.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TransRiwi.Views
+{
+    public class MainView
+    {
+        //Vista principal
+        public void ShowMenu()
+        {
+            Console.WriteLine("==== TransRiwi ====");
+            Console.WriteLine("1. Gestionar Clientes");
+            Console.WriteLine("2. Gestionar Vehículos");
+            Console.WriteLine("3. Gestionar Conductores");
+            Console.WriteLine("4. Gestionar Consultas");
+            Console.WriteLine("5. Salir");
+            int option;
+            while (!int.TryParse(Console.ReadLine(), out option) || option < 1 || option > 5)
+            {
+                System.Console.Write("Opcion invalida, intenta de nuevo: ");
+            }
+        }
+
+        //Solicitar un id para editar o eliminar
+        public Guid GetIdForAction(string action)
+        {
+            System.Console.Write($"Ingresa el id del registro a {action}: ");
+            Guid id;
+            while (!Guid.TryParse(Console.ReadLine(), out id))
+            {
+                Console.Write("Id invalido. Intente de nuevo: ");
+            }
+            return id;
+        }
+
+        //Monstrar mensaje y esperar confirmacion del usuario
+        public void ShowMessage(string mensaje)
+        {
+            System.Console.WriteLine(mensaje);
+            System.Console.WriteLine("Presione cualquier tecla para continuar...");
+            Console.ReadKey();
+        }
+    }
+}
