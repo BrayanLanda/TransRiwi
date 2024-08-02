497d5d48b172911de089681c251eaa9f86116938
Brayan Acosta <landakkpp@gmail.com>
2024-08-02 16:52:09 -0500

    creating app administrator and implementing crud in it. Creating Vehichel structure

---
+++ Models/AdministratorApp.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TransRiwi.Models
+{
+    public class AdministratorApp
+    {
+        //Listas 
+        public List<Customer> customers { get; set; } = new List<Customer>();
+        public List<Driver> drivers { get; set; } = new List<Driver>();
+        public List<Vehicle> vehicles { get; set; } = new List<Vehicle>();
+
+        //Metodos para agregar
+        public void AddCustomer(Customer customer)
+        {
+            customers.Add(customer);
+        }
+        public void AddDriver(Driver driver)
+        {
+            drivers.Add(driver);
+        }
+        public void AddVehicle(Vehicle vehicle)
+        {
+            vehicles.Add(vehicle);
+        }
+
+        //Metodos para eliminar
+        public void RemoveCustomer(Customer customer)
+        {
+            customers.Remove(customer);
+        }
+        public void RemoveDriver(Driver driver)
+        {
+            drivers.Remove(driver);
+        }
+        public void RemoveVehicle(Vehicle vehicle)
+        {
+            vehicles.Remove(vehicle);
+        }
+
+        //Metodos para mostrar 
+        public void ShowCustomers()
+        {
+            foreach (var customer in customers)
+            {
+                customer.SetShowDetails();
+            }
+        }
+        public void ShowDrivers()
+        {
+            foreach (var driver in drivers)
+            {
+                driver.SetShowDetails();
+            }
+        }
+        public void ShowVehicles()
+        {
+            foreach (var vehicle in vehicles)
+            {
+                //vehicle.SetShowDetails();
+            }
+        }
+
+        //Metodos para buscar
+        public Customer FindCustomerById(Guid id)
+        {
+            return customers.FirstOrDefault(c => c.GetId() == id);
+        }
+        public Driver FindDriverById(Guid id)
+        {
+            return drivers.FirstOrDefault(d => d.GetId() == id);
+        }
+        // public Vehicle FindVehicleById(Guid id)
+        // {
+        //     return//vehicles.FirstOrDefault(v => v.GetId() == id);
+        // }
+    }
+}
